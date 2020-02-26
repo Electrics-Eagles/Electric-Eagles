d965bbad4c4bcc5055df41960c3b10677914ad68
Alex Zaslavskis <sahsariga111@gmail.com>
2020-02-26 10:00:34 +0200

    Add Config

---
+++ Drone Code/Stabile Code/Modificated Code/config.h
@@ -0,0 +1,13 @@
+// Config File for Main Code 
+
+#define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
+#define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
+#define BARO_STABILITY 0
+#define ULTASONICS_SAFETY 0
+#define ULTROSONICS_WORKING_MODE 0
+#define interval 100
+#define MAX_PITCH_ANGLE 15
+#define MAX_ROLL_ANGLE 15
+#define DEBUG_MSG 1
+
+
